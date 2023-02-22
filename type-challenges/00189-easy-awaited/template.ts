c7a80c5e4445a831e610af073e7acad22e9f7ec6
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-02-22 21:58:22 +0800

    feat: add 00189-easy-awaited

---
+++ type-challenges/00189-easy-awaited/template.ts
@@ -0,0 +1,15 @@
+// type MyAwaited<T> = T extends PromiseLike<infer P>
+//   ? P extends PromiseLike<unknown>
+//     ? MyAwaited<P>
+//     : P
+//   : never;
+
+type MyAwaited<T extends object> = T extends Promise<infer P>
+  ? P extends Promise<any>
+    ? MyAwaited<P>
+    : P
+  : T extends object & {
+      then: (onfulfilled: (arg: infer P) => any) => any;
+    }
+  ? P
+  : never;
