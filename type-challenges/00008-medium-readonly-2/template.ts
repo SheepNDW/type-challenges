4950757acd63d00a6432f0a153b165ae8d52924e
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-03-10 09:17:15 +0800

    feat(MyReadonly2): update implementation
    使用 Readonly<Pick<T, K>> 來實作唯讀屬性

--- type-challenges/00008-medium-readonly-2/template.ts
+++ type-challenges/00008-medium-readonly-2/template.ts
@@ -1,6 +1,7 @@
-type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T, K> & {
-  readonly [P in keyof T]: T[P];
-};
+// type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T, K> & {
+//   readonly [P in keyof T]: T[P];
+// };
+type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T, K> & Readonly<Pick<T, K>>;
 
 // Omit<Type, Keys> 從 Type 中去除 Keys
 // & operater: 取交集
