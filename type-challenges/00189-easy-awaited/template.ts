c082492134f129e1d7d481b64458bc039e4a9c07
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-04-01 00:01:45 +0800

    feat(MyAwaited): update implementation

--- type-challenges/00189-easy-awaited/template.ts
+++ type-challenges/00189-easy-awaited/template.ts
@@ -1,15 +1,16 @@
-// type MyAwaited<T> = T extends PromiseLike<infer P>
-//   ? P extends PromiseLike<unknown>
-//     ? MyAwaited<P>
-//     : P
-//   : never;
-
-type MyAwaited<T extends object> = T extends Promise<infer P>
-  ? P extends Promise<any>
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer P>
+  ? P extends PromiseLike<unknown>
     ? MyAwaited<P>
     : P
-  : T extends object & {
-      then: (onfulfilled: (arg: infer P) => any) => any;
-    }
-  ? P
   : never;
+
+
+// type MyAwaited<T extends object> = T extends Promise<infer P>
+//   ? P extends Promise<any>
+//     ? MyAwaited<P>
+//     : P
+//   : T extends object & {
+//       then: (onfulfilled: (arg: infer P) => any) => any;
+//     }
+//   ? P
+//   : never;
