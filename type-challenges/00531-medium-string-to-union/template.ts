94bbc30cf8a6630a16cd03c7d5d9e727bdc8c605
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-04-06 09:34:00 +0800

    feat(StringToUnion): update implementation

--- type-challenges/00531-medium-string-to-union/template.ts
+++ type-challenges/00531-medium-string-to-union/template.ts
@@ -4,7 +4,7 @@ type StringToUnion<T extends string> =
   : never
 ;
 
-// Alternatives
+// ==== Alternatives ====
 // type StringToUnion<
 //   T extends string,
 //   Acc extends string[] = []
@@ -12,4 +12,14 @@ type StringToUnion<T extends string> =
 //   T extends `${infer Head}${infer Tail}`
 //   ? StringToUnion<Tail, [...Acc, Head]>
 //   : Acc[number]
+// ;
+
+// use tail recursion
+// type StringToUnion<
+//   T extends string,
+//   R = never
+// > =
+//   T extends `${infer Head}${infer Tail}`
+//   ? StringToUnion<Tail, R | Head>
+//   : R
 // ;
