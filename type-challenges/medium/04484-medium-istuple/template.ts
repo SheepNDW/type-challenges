ffa96e58640e7a5011e53ed55a69217a7691dced
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-06-05 09:41:18 +0800

    chore: add 04484-medium-istuple

---
+++ type-challenges/medium/04484-medium-istuple/template.ts
@@ -0,0 +1,28 @@
+type IsTuple<T> =
+  [T] extends [never]
+  ? false
+  : T extends readonly unknown[]
+    ? number extends T["length"]
+      ? false
+      : true
+    : false;
+
+// ==== Notes ====
+// when doing `number extends T['length']`:
+// - an array's "length" is `number`
+// - a tuple's "length" is a specific number literal
+
+
+// ==== Alternatives ====
+type IsAny<T> = 1 extends T & 0 ? true : false;
+// reference: 01042-medium-isnever
+// type IsNever<T> = [T] extends [never] ? true : false;
+
+type IsTuple1<T> =
+  true extends IsAny<T> | IsNever<T>
+  ? false
+  : T extends
+      | readonly [unknown, ...unknown[]]
+      | readonly []
+    ? true
+    : false;
