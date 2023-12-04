b79bdca7ca162be405c76a3819d9450d1ccbd0a4
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-12-04 09:47:18 +0800

    chore: update alternatives

--- type-challenges/hard/00147-hard-c-printf-parser/template.ts
+++ type-challenges/hard/00147-hard-c-printf-parser/template.ts
@@ -8,12 +8,36 @@ type ControlsMap = {
   p: 'pointer'
 }
 
-type ParsePrintFormat<T extends string> = 
-  T extends `${infer _}${infer Rest}`
-  ? T extends `${infer Head}%${infer Control}${infer Tail}`
-    ? Control extends keyof ControlsMap
-      ? [ControlsMap[Control], ...ParsePrintFormat<Tail>]
-      : ParsePrintFormat<Tail>
-    : ParsePrintFormat<Rest>
-  : []
-;
+// type ParsePrintFormat<T extends string> = 
+//   T extends `${infer _}${infer Rest}`
+//   ? T extends `${infer Head}%${infer Control}${infer Tail}`
+//     ? Control extends keyof ControlsMap
+//       ? [ControlsMap[Control], ...ParsePrintFormat<Tail>]
+//       : ParsePrintFormat<Tail>
+//     : ParsePrintFormat<Rest>
+//   : []
+// ;
+
+// ==== Alternatives ====
+// type ParsePrintFormat<T extends string> =
+//   T extends `${string}%${infer Control extends keyof ControlsMap}${infer Tail}`
+//   ? [ControlsMap[Control], ...ParsePrintFormat<Tail>]
+//   : T extends `${string}%${string}${infer Rest}`
+//     ? ParsePrintFormat<Rest>
+//     : [];
+
+
+// @teamchong
+type ParsePrintFormat<T extends string> =
+  // Get First 2 characters: %C
+  T extends `%${infer C extends keyof ControlsMap}${infer Rest}`
+  ? [ControlsMap[C], ...ParsePrintFormat<Rest>]
+
+    // Get First 2 characters: %?
+  : T extends `%${string}${infer Rest}`
+    ? ParsePrintFormat<Rest>
+    
+    // Get Next %? ${string}${string} to avoid matching string ends with %
+    : T extends `${string}%${string}${string}` & `${string}%${infer Rest}`
+      ? ParsePrintFormat<`%${Rest}`>
+      : [];
